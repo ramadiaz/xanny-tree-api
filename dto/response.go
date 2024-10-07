b4a56dd01cc485677b799370673316fdeeb39bc3
Rama Diaz <ramadiaz221@gmail.com>
2024-10-08 03:09:47 +0700

    MINOR UPDATE

--- dto/response.go
+++ dto/response.go
@@ -2,7 +2,7 @@ package dto
 
 type Response struct {
 	Status  int         `json:"status"`
-	Message string      `json:"message"`
+	Message string      `json:"message,omitempty"`
 	Body    interface{} `json:"body"`
 	Error   string      `json:"error,omitempty"`
 }
