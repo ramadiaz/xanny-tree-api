7eb687b89892a4174263d47e7745189a2743c64d
Rama Diaz <ramadiaz221@gmail.com>
2024-10-08 13:19:18 +0700

    UPDATE of GetUrl repo

--- repositories/tree_repo.go
+++ repositories/tree_repo.go
@@ -14,7 +14,13 @@ func (r *compRepository) RegisterUrl(data dto.Tree) error {
 func (r *compRepository) GetUrl(url string) (*string, error) {
 	var origin string
 
-	err := r.DB.QueryRow("SELECT original_url FROM tree_url WHERE short_url = $1 ORDER BY id DESC", url).Scan(&origin)
+	stmt, err := r.DB.Prepare("SELECT original_url FROM tree_url WHERE short_url = $1 ORDER BY id DESC")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	err = stmt.QueryRow(url).Scan(&origin)
 	if err != nil {
 		return nil, err
 	}
