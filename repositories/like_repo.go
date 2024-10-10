c395abd582ad2dba707a67d326e52cca4c5ce43e
Rama Diaz <ramadiaz221@gmail.com>
2024-10-11 02:11:43 +0700

    UPDATE of like_repo.go

--- repositories/like_repo.go
+++ repositories/like_repo.go
@@ -1,9 +1,11 @@
 package repositories
 
+import "time"
+
 func (r *compRepository) AddLike() (*int64, error) {
 	var total int64
 
-	err := r.DB.QueryRow("UPDATE like_count SET total = total + 1 WHERE name = 'profile' RETURNING total;").Scan(&total)
+	err := r.DB.QueryRow("UPDATE like_count SET total = total + 1, updated_at = $1 WHERE name = 'profile' RETURNING total;", time.Now()).Scan(&total)
 	if err != nil {
 		return nil, err
 	}
